a72f6b8e24b6de0fa5d51ef732217362cc5f4fe1
Khazaddoom <31121986g@gmail.com>
2023-10-27 18:37:55 +0530

    Debug statements to check env vlaue

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,13 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "../../../db";
+// import clientPromise from "../../../db";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ) {
     try {
-      await clientPromise;
-      res.status(200).json("Connected!")
+      // await clientPromise;
+      res.status(200).json(process.env.MONGODB_URI)
     } catch (error: any) {
       console.log(error)
       res.status(400).json({
